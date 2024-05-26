ce18eb84da20780980cce6baebcbddea1a5a41d4
Senichenkov <Petua41@yandex.ru>
2024-05-26 21:55:09 +0300

    Fix(Preprocessor): wrong bracket was highlighted if brackets didn't match

--- src/preprocessor/preprocessor.h
+++ src/preprocessor/preprocessor.h
@@ -19,6 +19,37 @@ private:
     /// @throw InvalidBracketsError -- if there's some problem with brackets, that cannot be solved
     void CheckBrackets() {
         // TODO(senichenkov): implement Preprocessor::CheckBrackets
+        // Check if brackets match:
+        short counter{0};
+        size_t first_open_bracket{0};
+        for (size_t i{0}; i < input_.size(); ++i) {
+            auto ch = input_[i];
+
+            if (ch == '(') {
+                if (first_open_bracket == 0) {
+                    first_open_bracket = i;
+                }
+                ++counter;
+            } else if (ch == ')') {
+                --counter;
+            }
+
+            if (counter == 0) {
+                // Reset this counter:
+                first_open_bracket = 0;
+            } else if (counter < 0) {
+                throw exceptions::InvalidBracketsError{std::move(input_), i,
+                                                       model::BracketsProblems::WasntOpened};
+            }
+        }
+
+        if (counter > 0) {
+            throw exceptions::InvalidBracketsError{std::move(input_), first_open_bracket,
+                                                   model::BracketsProblems::WasntClosed};
+        } else if (counter < 0) {
+            throw exceptions::InvalidBracketsError{std::move(input_), input_.size() - 1,
+                                                   model::BracketsProblems::WasntOpened};
+        }
 
         // FIXME(senichenkov): outermost brackets check should be smarter
         // Check outermost brackets:
@@ -27,31 +58,6 @@ private:
             if (input_.find(' ') != std::string::npos || input_.find('.') != std::string::npos) {
                 input_ = '(' + input_ + ')';
             }
-        }
-
-        // Check if brackets match:
-        short counter = 0;
-        for (size_t i{0}; i < input_.size(); ++i) {
-            auto ch = input_[i];
-
-            if (ch == '(') {
-                ++counter;
-            } else if (ch == ')') {
-                --counter;
-            }
-
-            if (counter < 0) {
-                throw exceptions::InvalidBracketsError{std::move(input_), i,
-                                                       model::BracketsProblems::WasntOpened};
-            }
-        }
-
-        if (counter > 0) {
-            throw exceptions::InvalidBracketsError{std::move(input_), 0,
-                                                   model::BracketsProblems::WasntClosed};
-        } else if (counter < 0) {
-            throw exceptions::InvalidBracketsError{std::move(input_), input_.size() - 1,
-                                                   model::BracketsProblems::WasntOpened};
         }
     }
 
