c303e1a1bf393fd4fbe889536012b116e8369461
Senichenkov <Petua41@yandex.ru>
2024-05-30 10:39:52 +0300

    Fix --strategy option description

--- cli/option_descriptions.h
+++ cli/option_descriptions.h
@@ -5,8 +5,7 @@ namespace cli::descriptions {
 constexpr auto kDNoBrackets = "don't add missing brackets";
 constexpr auto kDNoMacros = "don't replace macros with corresponding terms";
 constexpr auto kDRaw = "don't preprocess input. Combination of -b and -M";
-constexpr auto kDMaxOp = "maximum reduction operations before exit. 0 is infinity. Default is 0";
-constexpr auto kDStrategy = "reduction strategy to use. Default is NO";
+constexpr auto kDStrategy = "reduction strategy to use. Possible values: NO, APPL, CBN, CBV";
 constexpr auto kDHelp = "print this help and exit";
 constexpr auto kDTerm = "term to reduce";
 
--- cli/option_names.h
+++ cli/option_names.h
@@ -5,14 +5,12 @@ namespace cli::names {
 constexpr auto kNoBrackets = "no-brackets,b";
 constexpr auto kNoMacros = "no-macros,M";
 constexpr auto kRaw = "raw,r";
-constexpr auto kMaxOp = "max-operations,m";
 constexpr auto kStrategy = "strategy,s";
 constexpr auto kHelp = "help,h";
 
 constexpr auto kLongNoBrackets = "no-brackets";
 constexpr auto kLongNoMacros = "no-macros";
 constexpr auto kLongRaw = "raw";
-constexpr auto kLongMaxOp = "max-operations";
 constexpr auto kLongStrategy = "strategy";
 constexpr auto kLongHelp = "help";
 constexpr auto kLongTerm = "term";
